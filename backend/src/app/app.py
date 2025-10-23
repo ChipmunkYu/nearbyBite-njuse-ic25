89ed911fcec43d9a944375ee2b3480464a0fba48
ChipmunkYu <3380077139@qq.com>
2025-10-23 17:38:50 +0800

    feat: 完成推荐接口初步实现及自动化测试

--- backend/src/app/app.py
+++ backend/src/app/app.py
@@ -1,5 +1,6 @@
 
-from flask import Flask
+from flask import Flask, jsonify
+
 
 def create_app():
     app = Flask(__name__)
@@ -8,7 +9,24 @@ def create_app():
     def index():
         return {'message': 'Hello from Flask backend!'}
 
+    @app.route("/restaurants/recommend", methods=["GET"])
+    def recommend():
+        return jsonify([
+        {
+            "name": "麦当劳",
+            "location": "中山路",
+            "tags": ["快餐", "人均30"]
+        },
+        {
+            "name": "肯德基",
+            "location": "南大门口",
+            "tags": ["鸡肉", "人均35"]
+        }
+    ])
+
     return app
+
+
 
 if __name__ == '__main__':
     app = create_app()
