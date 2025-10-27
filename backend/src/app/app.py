4ad86f28a21ad99432498a2ed2d7494c37c153aa
KK <241250082@smail.nju.edu.cn>
2025-10-27 16:42:37 +0800

    define login and register interface and postman test successfully

--- backend/src/app/app.py
+++ backend/src/app/app.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 
 def create_app():
@@ -23,6 +23,37 @@ def create_app():
             "tags": ["鸡肉", "人均35"]
         }
     ])
+
+    # Registion interface (fake data now)
+    @app.route("/auth/register", methods=["POST"])
+    def register():
+        data = request.get_json()
+        username = data.get("username")
+        password = data.get("password")        
+    # 后续丰富判断逻辑
+        if not username :
+            return jsonify({"error": "用户名不能为空"}), 400
+        if not password :
+            return jsonify({"error": "密码不能为空"}), 400
+        
+        return jsonify({"message": "注册成功",
+                        "user" : {"username" : username}
+                        }), 201
+
+    # Login interface (fake data now)
+    @app.route("/auth/login", methods=["POST"])
+    def login():
+        data = request.get_json()
+        username = data.get("username")
+        password = data.get("password")
+        # 后续丰富判断逻辑
+        if username == "testuser" and password == "testpass":
+            return jsonify({"message": "登录成功",
+                            "token" : "fake-jwt-token"
+                            }), 200
+        else:
+            return jsonify({"error": "无效的用户名或密码"}), 401
+
 
     return app
 
