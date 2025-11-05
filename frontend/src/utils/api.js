f452a16e53c5eeece09cb53b631b34b0e132cc20
KK <241250082@smail.nju.edu.cn>
2025-11-06 00:48:09 +0800

    实现登录注册实际API

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,16 +2,19 @@ import axios from 'axios'
 
 // 创建 axios 实例
 const api = axios.create({
-    // Flask 默认端口，后续可改成相应的后端地址
-  baseURL: 'http://localhost:5000', 
+    // Flask 后端地址
+  baseURL: 'http://127.0.0.1:5000', 
   timeout: 5000,
 })
 
 //具体的 API 调用可以在这里定义，后续补充
 
-export function registerUser(userData) {
-  return api.post('/register', userData)
+export const register = (username, password) => {
+  return api.post('auth/register', { username, password })
 }
 
+export const login = (identifier, password) => {
+  return api.post('auth/login', { identifier, password })
+}
 
 export default api
