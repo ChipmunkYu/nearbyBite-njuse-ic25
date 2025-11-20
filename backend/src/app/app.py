a5fa594647457e4623a689eba44c6116db4f5589
KK <241250082@smail.nju.edu.cn>
2025-11-20 14:27:34 +0800

    同一分块处理

--- backend/src/app/app.py
+++ backend/src/app/app.py
@@ -5,10 +5,15 @@ from src.extensions import db, jwt, init_extensions
 #新表添加后请import（可以直接写在User后面或者另起一行重import）
 from src.models import User
 from src.routes.auth import auth_bp
+from src.routes.home import home_bp
+from src.routes.first import first_bp
+from src.routes.recommend import recommend_bp
 import os
 
  # 从 .env 文件加载环境变量
 from dotenv import load_dotenv
+
+
 
 def create_app():
     load_dotenv() 
@@ -17,33 +22,18 @@ def create_app():
     CORS(app)
 
     init_extensions(app)
-  
+    
+    # 注册蓝图
+    #路由在这里做了统一注册，router里面分块编写
+    
     app.register_blueprint(auth_bp)
+    app.register_blueprint(home_bp)
+    app.register_blueprint(first_bp)
+    app.register_blueprint(recommend_bp)
 
-    @app.route('/')
-    def index():
-        return {'message': 'Hello from Flask backend!'}
-
-    @app.route("/restaurants/recommend", methods=["GET"])
-    def recommend():
-        return jsonify([
-        {
-            "name": "麦当劳",
-            "location": "中山路",
-            "tags": ["快餐", "人均30"]
-        },
-        {
-            "name": "肯德基",
-            "location": "南大门口",
-            "tags": ["鸡肉", "人均35"]
-        }
-    ])
-
-   
-   
+  
    #数据库初始化
    # （普通功能的路由定义请写在这之前
-   # 想要分块一点写在routes文件夹，参考auth）
    
     with app.app_context():
          db.create_all()
