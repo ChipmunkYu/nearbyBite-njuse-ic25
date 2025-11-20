c905bd2e68e1a3b17c602161145ae8bff0124c8d
KK <241250082@smail.nju.edu.cn>
2025-11-20 14:48:06 +0800

    修改一些错误

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,4 +1,7 @@
 import axios from 'axios'
+import { ElMessage } from 'element-plus'
+import { useUserStore } from '@/stores/user' 
+
 
 // 创建 axios 实例
 const api = axios.create({
@@ -7,14 +10,28 @@ const api = axios.create({
   timeout: 5000,
 })
 
-//具体的 API 调用可以在这里定义，后续补充
+//具体的 API 调用可以在这里定义，后续补充,注意路径要和后端保持一致！！
 
 export const register = (username, password) => {
-  return api.post('auth/register', { username, password })
+  return api.post('api/auth/register', { username, password })
 }
 
 export const login = (identifier, password) => {
-  return api.post('auth/login', { identifier, password })
+  return api.post('api/auth/login', { identifier, password })
 }
+
+api.interceptors.response.use(
+  res => res,
+  err => {
+    const userStore = useUserStore()
+    if(err.response && err.response.status === 401) {
+      ElMessage.error('登录已过期，请重新登录～🍱')
+      userStore.logout()
+      router.push('/login')
+      
+    }
+    return Promise.reject(err)
+  }
+)
 
 export default api
