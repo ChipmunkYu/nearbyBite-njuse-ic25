d33470b050db1eb308c16e99a5ff51e8ff4d0fc3
KK <241250082@smail.nju.edu.cn>
2025-11-21 12:24:54 +0800

    解决冲突

--- backend/src/routes/auth.py
+++ backend/src/routes/auth.py
@@ -1,15 +1,8 @@
 from flask import Blueprint, request, jsonify
-<<<<<<< HEAD
-from ..extensions import db
-from ..models.user import User, generate_account_number
-from flask_jwt_extended import create_access_token
-=======
 from src.extensions import db
 from src.models.user import User, generate_account_number
 from flask_jwt_extended import create_access_token,  create_refresh_token
->>>>>>> 280fcfc5849cf76daef84436d931fff0a7eeed65
 from datetime import timedelta
-
 #注册：POST http://127.0.0.1:5000/api/auth/register
 
 #登录：POST http://127.0.0.1:5000/api/auth/login
@@ -75,9 +68,12 @@ def login():
         if not user.check_password(password):
             return jsonify({"code": 400, "message": "密码错误"}), 400
         
-<<<<<<< HEAD
-        token = create_access_token(identity = user.id, expires_delta = timedelta(hours = 4))
-        return jsonify({"code": 200, "message": "登录成功", "data": {"access_token": token, "user": user.to_dict()}}), 200
+        token = create_access_token(identity = str(user.id), expires_delta = timedelta(hours = 4))
+        refresh_token = create_refresh_token(identity=str(user.id), expires_delta=timedelta(days=7))
+
+        
+        return jsonify({"code": 200, "message": "登录成功", "data": {"access_token": token, "refresh_token": refresh_token, "user": user.to_dict()}}), 200
+
 
 #临时接口用于添加测试用户
 @auth_bp.route("/test/add-user", methods=["POST"])
@@ -90,10 +86,3 @@ def add_fake_user():
     db.session.add(user)
     db.session.commit()
     return jsonify({"message": "Test user added", "user_id": user.id}), 201
-=======
-        token = create_access_token(identity = str(user.id), expires_delta = timedelta(hours = 4))
-        refresh_token = create_refresh_token(identity=str(user.id), expires_delta=timedelta(days=7))
-
-        
-        return jsonify({"code": 200, "message": "登录成功", "data": {"access_token": token, "refresh_token": refresh_token, "user": user.to_dict()}}), 200
->>>>>>> 280fcfc5849cf76daef84436d931fff0a7eeed65
