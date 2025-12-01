8680a28d5ab2e1eb46d696c26fe455267160a27a
KK <241250082@smail.nju.edu.cn>
2025-12-02 01:18:25 +0800

    增加时间更新

--- backend/src/models/user.py
+++ backend/src/models/user.py
@@ -2,7 +2,7 @@
 import random
 import string
 from datetime import datetime
-from ..extensions import db
+from src.extensions import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime, timezone
 
@@ -10,15 +10,18 @@ def generate_account_number(k = 8):
     chars = string.ascii_uppercase + string.digits
     return ''.join(random.choices(chars, k = k))
 
+def utc_now():
+    return datetime.now(timezone.utc)
 class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key = True)
-    user_id = db.Column(db.String(20), unique = True, nullable = False)
+    user_id = db.Column(db.String(20), unique = True, nullable = False, default=generate_account_number)
     username = db.Column(db.String(80), unique = True, nullable = False)
     password_hash = db.Column(db.String(128), nullable = False)
     #这里采用 UTC 时间存储，确保时间的一致性，需要显示的时候转换为本地时间即可
-    created_at = db.Column(db.DateTime, default = datetime.now(timezone.utc))
+    created_at = db.Column(db.DateTime, default=utc_now)
+    updated_at = db.Column(db.DateTime, default=utc_now, onupdate=utc_now)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -28,7 +31,9 @@ class User(db.Model):
     
     def to_dict(self):
         return {
+            "id": self.id,
             "user_id": self.user_id,
             "username": self.username,
-            "created_at": self.created_at.isoformat() if self.created_at else None
+            "created_at": self.created_at.isoformat() if self.created_at else None,
+            "updated_at": self.updated_at.isoformat() if self.updated_at else None
         }
